3588d30b529fa3c0ad0151a41979210f1787d8b7
GakoTheCreator <gabrielribeiro043@gmail.com>
2026-09-10 23:50:44 -0300

    Add files via upload

---
+++ Relatorio 4/Exercicio 2/exer2.go
@@ -0,0 +1,27 @@
+package main
+import "fmt"
+
+func main() {
+	var trimestre1, trimestre2, trimestre3 int
+	fmt.Print("Digite as vendas do 1º trimestre: ")
+	fmt.Scanln(&trimestre1)
+	fmt.Print("Digite as vendas do 2º trimestre: ")
+	fmt.Scanln(&trimestre2)
+	fmt.Print("Digite as vendas do 3º trimestre: ")
+	fmt.Scanln(&trimestre3)
+	total := trimestre1 + trimestre2 + trimestre3
+	fmt.Println("Total de vendas:", total, "unidades")
+	if total < 100 {
+		fmt.Println("Meta mínima anual não atingida!")
+		return
+	}
+
+	switch {
+	case total >= 250:
+		fmt.Println("Classificação: Categoria Top Seller")
+	case total >= 180:
+		fmt.Println("Classificação: Categoria Sênior")
+	case total >= 100:
+		fmt.Println("Classificação: Categoria Pleno")
+	}
+}
